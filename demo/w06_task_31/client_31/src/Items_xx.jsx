851d983448e05d828a3d0cf56cda14eff324b239
soso1554848 <benz560701@gmail.com>
2024-03-27 22:05:29 +0800

    w06

---
+++ demo/w06_task_31/client_31/src/Items_xx.jsx
@@ -0,0 +1,27 @@
+import SingleItem_xx from './SingleItem_xx';
+import axios from 'axios';
+
+import { useQuery } from '@tanstack/react-query';
+
+//import customFetch_31 from './utils_31';
+
+const Items_xx = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: () => axios.get('http://localhost:5000/api/tasks'),
+  });
+  console.log('data', data);
+
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>;
+  }
+
+  return (
+    <div className='items'>
+      {data.data.taskList.map((item) => {
+        return <SingleItem_xx key={item.id} item={item} />;
+      })}
+    </div>
+  );
+};
+export default Items_xx;
---
+++ demo/w06_task_31/client_31/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App_xx from './App_xx';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App_xx />
+  </QueryClientProvider>
+);
